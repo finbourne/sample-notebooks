207840913d63801d6c98c29e8036b0f3aafb5a57
Rizwan Saeed <rizwan.saeed@finbourne.com>
2020-06-01 08:09:23 +0100

    DEV-8329 Generate index of notebooks based on docstring metadata

---
+++ docgen/__init__.py
@@ -0,0 +1,8 @@
+from docgen.nbmeta import NbMeta
+from docgen.parser import (
+    build_doc,
+    nb_relative_path,
+    parse,
+    process_nb,
+)
+
---
+++ docgen/nbmeta.py
@@ -0,0 +1,44 @@
+class NbMeta:
+    """
+    Metadata associated with a notebook.
+
+    Attributes
+    ----------
+
+    path : str
+        Relative folder path of the location of the notebook from the root of the repository
+    filename : str
+        Notebook filename
+    title : str
+        notebook title
+    description : str
+        Notebook description, taken from the short description of the docstring
+    features : [str]
+        List of notebook features, taken from the attributes of the docstring
+    formatted_features : str
+        Comma separated formatted string based on features
+
+    """
+
+    def __init__(self, path: str, filename: str, title: str, description: str, features: [str]):
+        """
+        Parameters
+        ----------
+        path : str
+            Relative folder path of the location of the notebook from the root of the repository
+        filename : str
+            Notebook filename
+        title : str
+            Notebook title
+        description : str
+            Notebook description, taken from the short description of the docstring
+        features : [str]
+            List of notebook features, taken from the attributes of the docstring
+        """
+
+        self.path = path
+        self.filename = filename
+        self.title = title
+        self.description = description
+        self.features = features
+        self.formatted_features = ", ".join(self.features)
