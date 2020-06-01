0378a62b285b9101acb56434b46a6dc2d2ac0013
Rizwan Saeed <rizwan.saeed@finbourne.com>
2020-06-01 08:09:23 +0100

    DEV-8329 Generate index of notebooks based on docstring metadata

---
+++ docgen/parser.py
@@ -0,0 +1,191 @@
+import chevron
+import docstring_parser as dp
+import itertools
+import nbformat
+import os
+
+from pathlib import Path
+
+from docgen import NbMeta
+
+
+def find_nbs(nb_root):
+    """
+    Traverses a folder looking for all .ipynb files and return them via a generator
+
+    Parameters
+    ----------
+    nb_root : str
+        Path to root folder containing notebooks to index
+
+    Returns
+    -------
+    str
+        Generator path to a discovered .ipynb file
+
+    """
+    for root, dirs, files in os.walk(nb_root, topdown=False):
+        for name in files:
+            if name.lower().endswith(".ipynb"):
+                yield os.path.join(root, name)
+
+
+def sanitize_docstring(raw_str):
+    """
+    Converts a formatted docstring and strips the leading and trailing triple "
+
+    Parameters
+    ----------
+    raw_str : str
+        Docstring containing triple "
+
+    Returns
+    -------
+    str
+        Docstring with leading and trailing triple " removed
+
+    """
+    stripped_str = raw_str.strip()
+    if stripped_str.startswith('"""') and stripped_str.endswith('"""'):
+        return stripped_str[3:-3]
+
+
+def nb_relative_path(abs_path):
+    """
+    Converts a full path to a relative path removing leading . directory navigation.
+    Used for formatting for display
+
+    Parameters
+    ----------
+    abs_path : str
+        Full path
+
+    Returns
+    -------
+    str
+        Relative path with directory navigation removed
+
+    """
+    rel_path = os.path.relpath(abs_path)
+    return rel_path[3:]
+
+
+def process_nb(nb_path):
+    """
+    Extracts metaadata from a notebook. By convention the metadata must:
+        - be the first cell
+        - be a code cell
+        - contain a short description
+        - contain an optional long description
+        - contain an optional list of features specified as Attributes
+
+    Parameters
+    ----------
+    nb_path : str
+        Path to notebook file
+
+    Returns
+    -------
+    NbMeta
+        Notebook metadata or None if unable to load metadata
+
+    """
+    nb = nbformat.read(nb_path, as_version=4)
+
+    if len(nb.cells) < 1 or nb.cells[0].cell_type != "code":
+        return
+
+    doc_str = sanitize_docstring(nb.cells[0].source)
+    doc_str_obj = dp.parse(doc_str)
+
+    if doc_str_obj.short_description is None:
+        return
+
+    return NbMeta(
+        nb_relative_path(os.path.dirname(nb_path)),
+        os.path.basename(nb_path),
+        doc_str_obj.short_description,
+        doc_str_obj.long_description,
+        sorted([m.arg_name for m in doc_str_obj.meta])
+    )
+
+
+def parse(nb_root):
+    """
+    Navigates a root folder to find all notebooks and extract their metadata where possible
+
+    Parameters
+    ----------
+    nb_root : str
+        Root folder containing notebooks
+
+    Returns
+    -------
+    [NbMeta]
+        List of NbMeta for notebooks that metadata was extracted for
+
+    """
+    return [
+        meta for meta in
+        [process_nb(nb) for nb in find_nbs(nb_root)]
+        if meta is not None
+    ]
+
+
+def build_doc(meta, template):
+    """
+    Generates a documentation index string given a list of metadata and mustache template
+
+    Parameters
+    ----------
+    meta : [NbMeta]
+        Notebook metadata
+    template : str
+        Path to mustache template for generating output documentation index. The template accepts
+        a single variable 'path' containing a list of dictionaries of path: [NbMeta in the path]
+
+    Returns
+    -------
+    str
+        Generated documentation index
+    """
+
+    # 1. group the notebooks by path
+    # 2. convert to a list of dictionaries for mustache
+    # 3. sort the notebooks alphabetically
+    # note: [*values] converts the generator to a list
+    nbs = [{"k": key, "v": sorted([*values], key=lambda m: m.filename)} for key, values in itertools.groupby(meta, lambda m: m.path)]
+
+    # sort by relative path
+    nbs.sort(key=lambda n: n["k"])
+
+    with open(template, 'r') as f:
+        return chevron.render(f, {'paths': nbs})
+
+
+def save_index_page(path, doc):
+    """
+    Saves the documentation index to a file
+
+    Parameters
+    ----------
+    path : str
+        Path to save the documentation index to
+    doc : str
+        Documentation index
+    """
+    with open(path, "w") as file:
+        file.write(doc)
+
+
+def main():
+    nb_root = Path(__file__).parent.parent.joinpath("examples")
+
+    meta = parse(nb_root=nb_root)
+    doc = build_doc(meta, 'README.mustache')
+
+    save_index_page(nb_root.joinpath("README.md"), doc)
+
+
+if __name__ == "__main__":
+    main()
---
+++ docgen/tests/test_docgen.py
@@ -0,0 +1,69 @@
+import unittest
+from pathlib import Path
+
+from docgen import (
+    NbMeta,
+    build_doc,
+    process_nb,
+    nb_relative_path)
+
+from parameterized import parameterized
+
+
+class DocGenTests(unittest.TestCase):
+
+    @parameterized.expand(
+        [
+            ["notebooks/no_features.ipynb", "Notebook title", "Longer description of notebook"],
+            ["notebooks/no_description.ipynb", "Notebook title", None, ["derived portfolios",
+                                                                        "properties",
+                                                                        "transaction config",]],
+            ["notebooks/valid.ipynb", "Notebook title", "Longer description of notebook", ["derived portfolios",
+                                                                                           "properties",
+                                                                                           "transaction config",]]
+        ]
+    )
+    def test_parse_metadata(self, nb, title=None, description=None, features=[]):
+        meta = process_nb(nb)
+
+        self.assertEqual(meta.title, title)
+        self.assertEqual(meta.description, description)
+        self.assertListEqual(meta.features, features)
+
+    @parameterized.expand(
+        [
+            "notebooks/markdown_cell.ipynb",
+            "notebooks/no_docstring.ipynb"
+        ]
+    )
+    def test_invalid_metadata(self, nb):
+        meta = process_nb(nb)
+        self.assertIsNone(meta)
+
+    def test_build_doc(self):
+        meta = [
+            NbMeta("a/b", "b.ipynb", "title ab", "des ab", ["a", "b"]),
+            NbMeta("c", "c.ipynb", "title c", "des c", ["c"]),
+            NbMeta("a", "a.ipynb", "title a", "des a", ["a"]),
+        ]
+
+        template = Path(__file__).parent.parent.joinpath("README.mustache")
+
+        doc = build_doc(meta, template)
+
+        self.assertIn("| a.ipynb | title a | des a | a |", doc)
+        self.assertIn("| b.ipynb | title ab | des ab | a, b |", doc)
+        self.assertIn("| c.ipynb | title c | des c | c |",  doc)
+
+        print(doc)
+
+    def test_nb_relative_path(self):
+
+        nb_root = Path(__file__).parent.parent.joinpath("examples").joinpath("use-cases").joinpath("ibor")
+        rel_path = nb_relative_path(nb_root)
+
+        self.assertEquals(rel_path, "examples/use-cases/ibor")
+
+
+if __name__ == '__main__':
+    unittest.main()
