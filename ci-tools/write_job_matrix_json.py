2d237fd13f159093f07b8483e8b7fa3d9bb2a996
Stephen LM <stephen.lemasney@finbourne.com>
2021-06-22 16:53:54 +0100

    SE-1426: testing

---
+++ ci-tools/write_job_matrix_json.py
@@ -0,0 +1,53 @@
+import os
+import json
+from pathlib import Path
+
+def find_nbs(nb_root):
+    """
+    Traverses a folder looking for all .ipynb files and return them via a generator
+    Parameters
+    ----------
+    nb_root : str
+        Path to root folder containing notebooks to index
+    Returns
+    -------
+    str
+        Generator path to a discovered .ipynb file
+    """
+
+    nbs_dirs = []
+
+    for root, dirs, files in os.walk(nb_root, topdown=False):
+        if ".ipynb_checkpoints" not in root:
+            for name in files:
+                if name.lower().endswith(".ipynb"):
+                    nbs_dirs.append(os.path.join(root))
+    
+    return set(nbs_dirs)
+
+
+def construct_matrix_json(nb_dirs):
+
+    matrix_json = []
+    for notebook_dir in nb_dirs:
+        matrix_json.append({"notebookDir": notebook_dir})
+    
+    return matrix_json
+
+def main():
+
+    root_nb_path = Path(__file__).absolute().parent.parent.joinpath("examples")
+
+    nb_dirs = find_nbs(root_nb_path)
+
+    matrix_json = construct_matrix_json(nb_dirs)
+
+    matrix_json_path = Path(__file__).absolute().parent.parent.joinpath("ci-tools").joinpath("job_matrix.json")
+
+    print(matrix_json_path)
+
+    with open(matrix_json_path, "w") as file:
+        json.dump(matrix_json, file)
+
+if __name__ == "__main__":
+    main()
